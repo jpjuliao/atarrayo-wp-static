1d50052859f670d102eec2c54c683a1cf5a776db
Juan Pablo Juliao <jpjuliao@gmail.com>
2024-03-10 22:43:02 -0500

    Add social links

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Remove all static site files except development-related files
+find . -mindepth 1 -maxdepth 1 \( ! -name .git -a ! -name .gitignore -a ! -name static-site -a ! -name README.md ! -name generate.sh ! -name deploy.sh \) -exec rm -r {} \;\n
+
+# Copy newly generated static site files to the root folder
+cp -r static-site/* .
+
 # Check if message argument is provided
 if [ -z "$1" ]; then
     echo "Error: Please provide a commit message."
